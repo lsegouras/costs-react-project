f8381c45cbc6625b315cfb37985ea3406dee384a
Laura Segouras <lausegouras@yahoo.com.br>
2023-03-04 11:58:48 -0300

    Pages, Routes and CSS

---
+++ src/components/pages/Company.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Company extends Component {
+  render() {
+    return (
+      <div>Company</div>
+    )
+  }
+}
+
+export default Company
---
+++ src/components/pages/NewProject.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class NewProject extends Component {
+  render() {
+    return (
+      <div>NewProject</div>
+    )
+  }
+}
+
+export default NewProject
