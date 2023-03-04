ca67c7415db30fe980bde94f936886d245562b83
Laura Segouras <lausegouras@yahoo.com.br>
2023-03-04 10:47:50 -0300

    Project Set Up

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <ul>
+        <li>Home</li>
+        <li>Empresa</li>
+        <li>Contato</li>
+        <li>Novo Projeto</li>
+      </ul>
+
+      <p>Footer</p>
+
+      <Routes>
+      
+      <Route></Route>
+
+
+      </Routes>
+
+
+    </Router>
+  );
+}
+
+export default App;
