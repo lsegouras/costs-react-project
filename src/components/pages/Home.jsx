9cf1c3884638ac437bf32447c4e196c8d68a360d
Laura Segouras <lausegouras@yahoo.com.br>
2023-03-07 13:46:11 -0300

    Creating Home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+import LinkButton from '../layouts/LinkButton'
+
 
 export class Home extends Component {
   render() {
     return (
-      <h1>Home</h1>
+      <section className={styles.home_container}>
+        <h1>Bem-vindo ao <span>Costs</span></h1>
+        <p>Comece a gerenciar seus projetos agora mesmo!</p>
+        <LinkButton to="/newproject" text="Criar Projeto"/>
+        <img src={savings} alt="savings svg"></img>
+      </section>
     )
   }
 }
